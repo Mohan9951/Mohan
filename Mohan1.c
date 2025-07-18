7f06824f6cac736f168b2d4e5dd409c32ad3876d
Mohan123 <dharavathmohan8882@gmail.com>
2025-07-18 10:05:39 +0530

    firstcommit

---
+++ Mohan1.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+int A,B,C
+printf("enter A marks");
+scanf("%d", A)  
+printf("enter B marks");
+scanf("%d", B)  
+printf("enter C marks");
+scanf("%d", C)  
+printf("enter A+B+C marcks")
+scanf("%d", A+B+C);
+
+return 0;
+}
